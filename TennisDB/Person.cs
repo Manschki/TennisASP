38bd7515cb0116d5f7c463715c85f04554380f41
Manschki <seifriedsbergerm0123@gmail.com>
2020-05-29 10:47:24 +0200

    Init

---
+++ TennisDB/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TennisDB
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public List<Booking> Bookings { get; set; }
+    }
+}
---
+++ TennisDB/TennisContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TennisDB
+{
+    public class TennisContext : DbContext
+    {
+        public TennisContext() { }
+        public TennisContext(DbContextOptions<TennisContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "data source=(LocalDB)\\mssqllocaldb;attachdbfilename=C:\\Temp\\NorthwindWebAPIDb\\Tennis.mdf;integrated security=True";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+    }
+}
